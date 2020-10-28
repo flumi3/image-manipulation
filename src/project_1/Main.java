b1ba844dc9c3f0cf20efc7420493d87b0f0dbeff
Sebastian Flum <sebastian.flum@hotmail.de>
2020-10-28 11:37:27 +0100

    Edited some names

--- src/project_1/Main.java
+++ src/project_1/Main.java
@@ -5,42 +5,42 @@ import java.util.ArrayList;
 public class Main {
 
     // Exercise 1
-    private static void taskOne(ImageManipulation man) {
+    private static void exerciseOne(ImageManipulation man) {
         ArrayList<Pixel> pixels = man.getPixels();
         pixels = man.removeBlueTones(pixels);
         man.createPPM(pixels, "./src/project_1/aufgabe_1.ppm");
     }
 
     // Exercise 2
-    private static void taskTwo(ImageManipulation man) {
+    private static void exerciseTwo(ImageManipulation man) {
         ArrayList<Pixel> pixels = man.getPixels();
         pixels = man.increaseBlue(pixels);
         man.createPPM(pixels, "./src/project_1/aufgabe_2.ppm");
     }
 
     // Exercise 3
-    private static void taskThree(ImageManipulation man) {
+    private static void exerciseThree(ImageManipulation man) {
         ArrayList<Pixel> pixels = man.getPixels();
         pixels = man.increaseAll(pixels);
         man.createPPM(pixels, "./src/project_1/aufgabe_3.ppm");
     }
 
     // Exercise 4
-    private static void taskFour(ImageManipulation man) {
+    private static void exerciseFour(ImageManipulation man) {
         ArrayList<Pixel> pixels = man.getPixels();
         man.changeMaxColorValue(pixels,5);
         man.createPPM(pixels, "./src/project_1/aufgabe_4.ppm");
     }
 
     // Exercise 5
-    private static void taskFive(ImageManipulation man) {
+    private static void exerciseFive(ImageManipulation man) {
         ArrayList<Pixel> pixels = man.getPixels();
         ArrayList<Integer> values = man.convertToPGM(pixels);
         man.createPGM(values, "./src/project_1/aufgabe_5.pgm");
     }
 
     // Exercise 6
-    private static void taskSix(ImageManipulation man) {
+    private static void exerciseSix(ImageManipulation man) {
         man.createPBM("./src/project_1/aufgabe_6.pgm");
     }
 
@@ -52,12 +52,12 @@ public class Main {
         // create new image manipulator for the given picture
         ImageManipulation man = new ImageManipulation(path);
 
-        // execute tasks
-        taskOne(man);
-        taskTwo(man);
-        taskThree(man);
-        taskFour(man);
-        taskFive(man);
-        taskSix(man);
+        // execute the exercises
+        exerciseOne(man);
+        exerciseTwo(man);
+        exerciseThree(man);
+        exerciseFour(man);
+        exerciseFive(man);
+        exerciseSix(man);
     }
 }
